448857ce8f162a5aabb103cb438fc34e5fc19ca6
Kevin COOVI <k.coovi@gmail.com>
2023-03-16 08:10:09 -0700

    A Write a C program that prints something

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exactly something
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
