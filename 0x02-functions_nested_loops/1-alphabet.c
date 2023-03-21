e0c578694a91dcc7aa10ff83f5a28af355b8484e
Kevin COOVI <k.coovi@gmail.com>
2023-03-21 15:41:37 +0300

    A unction that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,14 @@
  * main - whatever
  * Return: 0
  */
-void print_alphabet(void) {
-  char letter = 'a';
-  
-  while (letter <= 'z') {
-    _putchar(letter);
-    letter++;
-  }
-  
-  _putchar('\n');
-}
+void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
