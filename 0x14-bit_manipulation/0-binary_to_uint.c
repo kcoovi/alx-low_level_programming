a94d6a7703f8b5189efca478ec5728e6f75983a6
Kevin COOVI <k.coovi@gmail.com>
2023-05-04 22:44:05 +0300

    A function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,11 +16,11 @@ unsigned int i = 0;
 if (b == NULL)
 return (0);
 
-	for ( a = 0; b[a]; a++)
+	for (a = 0; b[a]; a++)
 	{
 		if (b[a] < '0' || b[a] > '1')
 		return (0);
-	i = 2 *i + b[a] - '0';
+	i = 2 * i + b[a] - '0';
 
 	}
 return (i);
