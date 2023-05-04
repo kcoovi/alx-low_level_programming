8e1a39012120a76888416b89fe0a96e45eaeffc0
Kevin COOVI <k.coovi@gmail.com>
2023-05-04 22:42:10 +0300

    A function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - function
+ * @b: string
+ * Return: return
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+int a;
+unsigned int i = 0;
+
+if (b == NULL)
+return (0);
+
+	for ( a = 0; b[a]; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+		return (0);
+	i = 2 *i + b[a] - '0';
+
+	}
+return (i);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+# C - Bit Manipulation
+## General
+* How to manipulate bits and use bitwise operators
