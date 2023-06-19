b40db15a873de1627ea50d619efb223a7b5f0f79
Kevin COOVI <k.coovi@gmail.com>
2023-06-19 14:34:48 +0300

    C Functions

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - addition
+ * @a: num 1
+ * @b: nul 2
+ * Return: operation
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: num 1
+ * @b: nul 2
+ * Return: operation
+ */
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: num 1
+ * @b: nul 2
+ * Return: operation
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - division
+ * @a: num 1
+ * @b: nul 2
+ * Return: operation
+ */
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - modulo
+ * @a: num 1
+ * @b: nul 2
+ * Return: operation
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
