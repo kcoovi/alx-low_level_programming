cfaf259916ac883391e5a27417329fd3a45ffb6f
Kevin COOVI <k.coovi@gmail.com>
2023-06-19 14:52:37 +0300

    Get winning numbers

---
+++ 0x18-dynamic_libraries/win.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * get_win_numbers - Get the winning numbers
+ * @numbers: the numbers
+ */
+void get_win_numbers(int *numbers)
+{
+    numbers[0] = 9;
+    numbers[1] = 8;
+    numbers[2] = 10;
+    numbers[3] = 24;
+    numbers[4] = 75;
+    numbers[5] = 9;
+}
