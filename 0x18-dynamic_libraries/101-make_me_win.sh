3dd504652e5263bb917c5495793d3578d363dbe8
Kevin COOVI <k.coovi@gmail.com>
2023-06-19 15:15:09 +0300

    Code Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/kcoovi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+wget -O win.so https://github.com/kcoovi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=$(pwd)/win.so
