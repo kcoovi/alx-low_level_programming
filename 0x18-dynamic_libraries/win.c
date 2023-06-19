8241fc748e3969af17ef25bb1df2166feeda2841
Kevin COOVI <k.coovi@gmail.com>
2023-06-19 15:02:42 +0300

    Get winning numbers

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -8,10 +8,12 @@
  */
 void get_win_numbers(int *numbers)
 {
-    numbers[0] = 9;
-    numbers[1] = 8;
-    numbers[2] = 10;
-    numbers[3] = 24;
-    numbers[4] = 75;
-    numbers[5] = 9;
+    
+numbers[0] = 9;
+numbers[1] = 8;
+numbers[2] = 10;
+numbers[3] = 24;
+numbers[4] = 75;
+numbers[5] = 9;
+
 }
